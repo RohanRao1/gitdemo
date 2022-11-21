2f811650c1ae86036899cb2a2f1597db5303b960
Rohanrao1 <rohanrao389@gmail.com>
2022-11-22 01:55:58 +0530

    dom basics

---
+++ dom.js
@@ -0,0 +1,61 @@
+// //Examine Document object
+
+ // //console.dir(document)
+// console.log(document.domain)
+// console.log(document.URL)
+// console.log(document.title)
+// //document.title = 123
+// console.log(document.doctype)
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.all)
+// console.log(document.all[10])
+// //document.all[10].textContent = "Hi Rohan"
+// console.log(document.forms)
+// console.log(document.links)
+//console.log(document.images)
+
+//GETELEMENTBYID
+//console.log(document.getElementById('header-title'))
+var headertitle = document.getElementById('header-title')
+// var header = document.getElementById('main-header')
+// //console.log(headertitle)
+// //headertitle.textContent = 'Hi'
+// //headertitle.innerText = 'Rohan'
+// //console.log(headertitle.textContent)
+// //console.log(headertitle.innerText)
+// //headertitle.innerHTML = '<h3>Hello</h3>'
+// headertitle.style.borderBottom = '3px solid black'
+// header.style.border = '3px solid #000'
+headertitle.style.fontWeight = "bold"
+headertitle.style.color = "green"
+
+
+// GETELEMENTSBYCLASSNAME 
+
+/*var items = document.getElementsByClassName('list-group-item')
+console.log(items)
+console.log(items[1])
+items[1].textContent = "hello"
+items[1].style.fontWeight = "bold"
+items[1].style.backgroundColor = "yellow"
+
+//items.style.backgroundColor = '#f4f4f4'
+for(var i = 0 ; i<items.length; i ++){
+    items[i].style.backgroundColor = '#f4f4f4'
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
