b885fbc6d9d2eb90a9aa584ddd79c4d9ff48e110
Rohanrao1 <rohanrao389@gmail.com>
2022-11-23 17:38:52 +0530

    getelementbyclass

---
+++ dom task 4/getbyclassname.js
@@ -0,0 +1,31 @@
+
+// GETELEMENTSBYCLASSNAME 
+
+var items = document.getElementsByClassName('list-group-item')
+//console.log(items)
+//console.log(items[1])
+//items[1].textContent = "hello"
+//items[1].style.fontWeight = "bold"
+//items[1].style.backgroundColor = "yellow"
+items[2].style.backgroundColor = "green"
+
+//items.style.backgroundColor = '#f4f4f4'
+for(var i = 0 ; i<items.length; i ++){
+    //items[i].style.backgroundColor = '#f4f4f4'
+    items[i].style.fontWeight = "bold"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
