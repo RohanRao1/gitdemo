526e4ce05b7bd4f5bfc14b81cc53a4e91883a253
Rohanrao1 <rohanrao389@gmail.com>
2022-12-06 14:39:23 +0530

    checking using other method

--- regform.js
+++ regform.js
@@ -7,3 +7,18 @@ function savetolocal(event){
     localStorage.setItem('email', email)
     localStorage.setItem('phone number', num)
 }
+
+
+/*var form = document.getElementById('addform')
+form.addEventListener('submit',store)
+
+function store(e){
+    e.preventDefault()
+    var name = document.getElementById('name').value
+    var email = document.getElementById('mail').value
+    var num = document.getElementById('phno').value
+    localStorage.setItem('name',name)
+    localStorage.setItem('email',email)
+    localStorage.setItem('number',num)
+}
+*/
